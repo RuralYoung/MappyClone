2f8ec406d7605ef528e9e536b86b39181c0bf027
Rural Young <ruralyoungiii@gmail.com>
2024-06-19 23:06:24 -0400

    Made Some Changes to Create a Better Basis for Development

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var speed = 300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	velocity.x = Input.get_axis("move_left", "move_right") * speed
+	
+	move_and_slide()
